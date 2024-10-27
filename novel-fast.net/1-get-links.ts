a643f92160d48e457557be0c58641e433dd30752
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-10-28 06:15:59 +0700

    *

---
+++ novel-fast.net/1-get-links.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import fs from "fs";
+import * as cheerio from "cheerio";
+import data from "./1-get-links.json";
+
+const main = async (data: any) => {
+  for (let i = 0; i < data.length; i++) {
+    const response = await axios.get(data[i]);
+    const $ = cheerio.load(response.data);
+
+    const links = $("#post-177920 > div.sertobody > div.bixbox.bxcl.epcheck > div.eplister.eplisterfull > ul > li > a");
+
+    let l = [];
+    for (let i = 0; i < links.length; i++) {
+      const link = $(links[i]).attr("href");
+      l.push(link);
+
+      // write to one-page.data.json file use es6 version
+      fs.writeFileSync("./novel-fast.net/get-content.json", JSON.stringify(l));
+    }
+  }
+};
+
+main(data);
--- novel-fast.net/2-get-content.ts
+++ novel-fast.net/2-get-content.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-import data from "./one-page-data.json";
+import data from "./2-get-content.json";
 
 const main = async (data: string[]) => {
   for (let i = 0; i < data.length; i++) {
